a1fe16d790578f92fcfbd1bc95de67d91d6026fe
lrlm12 <70547723+lrlm12@users.noreply.github.com>
2024-06-14 00:38:59 +0000

    Pending changes exported from your codespace

---
+++ src/Comp1.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function Uno()
+{
+    return(
+    <div className="caja1">
+        <div className="texto" >
+        <h1>COMPONENTE DE PRUEBA</h1>
+        <p>Esto reduce la probabilidad de que ocurran problemas relacionados con el medio ambiente y que sean difíciles de depurar</p>
+      </div>   
+      <div className="imagen" >
+       <img src="Octocat.png" className="imagen"  ></img>
+        </div>
+    </div>
+
+        
+    );
+    
+}
+export default Uno;
--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Uno from './Comp1.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Uno /> 
   </React.StrictMode>
 );
 
